abeb2cb2a01a705b348f42721ee26d8b2c09511e
UgoMango <ugoniniappakademie@gmail.com>
2023-09-13 18:59:55 +0200

    Class FitnessPage created

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,281 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(initialRoute: "/main", routes: {
+    "/main": (context) => const MyApp(),
+    "/fitness": (context) => const FitnessPage(),
+    "/profile": (context) => const ProfilePage(),
+    "/chat": (context) => const ChatPage(),
+  }));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stay Healthy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color(0xFFCA411B),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "Hallo Ugo"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static final List<Widget> _widgetOptions = <Widget>[
+    const FitnessTracker(index: 0),
+    const FitnessPage(),
+    const ProfilePage(),
+    const ChatPage(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run),
+            label: 'Fitness',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class FitnessTracker extends StatefulWidget {
+  final int index;
+  const FitnessTracker({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  State<FitnessTracker> createState() => _FitnessTrackerState();
+}
+
+class _FitnessTrackerState extends State<FitnessTracker> {
+  List<int> counters = [0, 0, 0];
+
+  void _incrementCounter(int i) {
+    setState(() {
+      i = 0;
+      counters[i]++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const text = Text("Fitness Tracker");
+    return Scaffold(
+      appBar: AppBar(
+        title: text,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 36),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TrackingElement(
+              color: const Color.fromARGB(255, 222, 237, 11),
+              iconData: Icons.directions_run,
+              unit: 'm',
+              onTap: () {
+                _incrementCounter(0);
+              },
+              counter: (0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 20),
+            TrackingElement(
+              color: const Color.fromARGB(255, 17, 50, 234),
+              iconData: Icons.local_drink,
+              unit: 'l',
+              onTap: () {
+                _incrementCounter(0);
+              },
+              counter: (1),
+            ),
+            const SizedBox(height: 20),
+            TrackingElement(
+              color: const Color.fromARGB(255, 9, 240, 79),
+              iconData: Icons.fastfood,
+              unit: 'cal',
+              onTap: () {
+                _incrementCounter(2);
+              },
+              counter: (2),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TrackingElement extends StatelessWidget {
+  final Color color;
+  final IconData iconData;
+  final String unit;
+  final VoidCallback onTap;
+  final int counter;
+
+  const TrackingElement({
+    Key? key,
+    required this.color,
+    required this.iconData,
+    required this.unit,
+    required this.onTap,
+    required this.counter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 11),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  iconData,
+                  color: Colors.black,
+                  size: 55,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  '$counter/ 5000 $unit',
+                  style: const TextStyle(color: Colors.black, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          LinearProgressIndicator(
+            value: counter / 5000,
+            backgroundColor: Colors.white,
+            minHeight: 10.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FitnessPage extends StatelessWidget {
+  const FitnessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fitness Videos"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Fitness Videos anzeigen und an Live-Kursen teilnehmen',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profil"),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 36),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Profil erstellen, Profilbild hochladen oder mit der Kamera aufnehmen.',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chat"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Chat-Raum',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
